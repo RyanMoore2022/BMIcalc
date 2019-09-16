a221d588c96aa1f2a6337ef9c2f9086fad731870
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-09-16 09:56:55 -0400

    added value, variables need fixed

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,20 +7,20 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner keyboard;
-
         keyboard = new Scanner(System.in);
-    String bmi;
-    String m;
-    String inch;
-    String kg;
-    String pounds;
+
+    int bmi;
+    int m;
+    int inch;
+    int kg;
+    int pound;
 
 
-
-
-
-
-
+    bmi = kg/m*m;
+    m = inch*(0.0254/1);
+    inch = m*(1/0.0254);
+    kg = pound*(2.20462);
+    pound = kg*(1/2.20462);
 
 
 
