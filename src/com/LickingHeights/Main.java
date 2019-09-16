65db5f78bf97aa03e4edd3fb183049deab0a004d
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-09-16 09:47:43 -0400

    added scanner and some variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,13 +1,29 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner keyboard;
+
+        keyboard = new Scanner(System.in);
+    String bmi;
+    String m;
+    String inch;
+    String kg;
+    String pounds;
 
 
 
-        
+
+
+
+
+
+
+
 
     }
 }
