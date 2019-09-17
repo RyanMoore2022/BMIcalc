2fefe0d91080a9f8a541547e3c54550a1d716043
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-09-17 09:25:31 -0400

    added questions and fixed variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,21 +9,19 @@ public class Main {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
-    int bmi;
-    int m;
-    int inch;
-    int kg;
-    int pound;
+    String name;
+    Double inch;
+    Double pound;
 
+    System.out.println("Hello, what is your name?");
+        name = keyboard.nextLine();
+    System.out.println("Hello " +name);
+    System.out.println("To calculate your Body Mass Index, just answer these questions.");
 
-    bmi = kg/m*m;
-    m = inch*(0.0254/1);
-    inch = m*(1/0.0254);
-    kg = pound*(2.20462);
-    pound = kg*(1/2.20462);
-
-
-
+    System.out.println("What is your height in inches?");
+        inch = keyboard.nextDouble();
+    System.out.println("What is your weight in pounds?");
+        pound = keyboard.nextDouble();
 
     }
 }
